ec638c872662d81fe28ad492cd49122e3dbb42a2
Konstantin Münster <konstantin.muenster@mail.com>
2020-10-10 10:42:25 +0200

    Add job to delete all inactive users

---
+++ backend/jobs/deleteInactiveUsers.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const User = require("../models/user");
+
+// In the sign up process, users get an email to activate their
+// account. If they do not activate it within 48 hours, this job
+// removes any inactive user from the database.
+
+const TWO_DAYS = 1000 * 60 * 60 * 24 * 2;
+
+const deleteInactiveUsers = async () => {
+  console.log(`Starting job: ${deleteInactiveUsers.name}`);
+  try {
+    const response = await User.deleteMany({
+      active: false,
+      createdAt: { $lt: new Date(Date.now() - TWO_DAYS).toISOString() },
+    });
+    console.log(`SUCCESS - Deleted ${response.deletedCount} users.`);
+  } catch (err) {
+    console.log(`ERROR - ${err.message}`);
+  }
+};
+
+module.exports = deleteInactiveUsers;
---
+++ backend/jobs/index.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const deleteInactiveUsers = require("./deleteInactiveUsers");
+
+const executeJobs = async () => {
+  console.log("Setup database connection");
+  mongoose
+    .connect(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@notion-clone-0.gqqwp.gcp.mongodb.net/notion?retryWrites=true&w=majority`,
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(async () => {
+      console.log("SUCCESS - Database connected.");
+
+      // Running jobs
+      await deleteInactiveUsers();
+
+      process.exit();
+    })
+    .catch((err) => {
+      console.log(`ERROR - While connected to database: ${err.message}`);
+    });
+};
+
+executeJobs();
